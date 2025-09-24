60064ddf993a2c077f772f1efdbd0cab19dadcbb
Yherco Yhafar Espinoza Ticona <yherco11111@gmail.com>
2025-09-24 13:32:29 -0400

    Update login.php

--- capaPresentacion/vistas/M2_seguridad_roles_auditoria/login.php
+++ capaPresentacion/vistas/M2_seguridad_roles_auditoria/login.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/estilo_login.css">
+    <link rel="stylesheet" href="../assets/css/estilo_login.css">
     <title>Login</title>
 </head>
 <body>
