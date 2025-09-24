307e0f8ce13f927a8c7b89dfa570161d28b4d145
Yherco Yhafar Espinoza Ticona <yherco11111@gmail.com>
2025-09-24 13:50:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once("capaPresentacion/vistas/M2_seguridad_roles_auditoria/login.php");
+// require_once("capaPresentacion/vistas/M2_seguridad_roles_auditoria/login.php");
 
-
+echo "hola estoy en linea";
 
 ?>
