5b3679f6a3f87929adbb286ce215e03c5775d940
Yherco Yhafar Espinoza Ticona <yherco11111@gmail.com>
2025-09-24 13:12:02 -0400

    Update usuario.php

--- capaDatos/DAOs/M2_seguridad_roles_aditoria/usuario.php
+++ capaDatos/DAOs/M2_seguridad_roles_aditoria/usuario.php
@@ -1,3 +1,29 @@
 <?php
-// Modelo para gestión de artículos
+class Conexion {
+    private $host = "localhost";
+    private $dbname = "bd_articulos";
+    private $user = "root";
+    private $pass = "";
+    private $conn;
+
+    public function __construct() {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
+           
+            $this->conn = new PDO($dsn, $this->user, $this->pass);
+      
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $this->conn->exec("set names utf8");
+            
+        } catch (PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
+            die();
+        }
+    }
+
+    public function getConexion() {
+        return $this->conn;
+    }
+}
 ?>
